b382fd8f09c2df03c50750332c06626054f430fa
hyperbala <balagopal123@gmail.com>
2024-12-21 19:39:38 +0530

    build

---
+++ next.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone', 
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
+
